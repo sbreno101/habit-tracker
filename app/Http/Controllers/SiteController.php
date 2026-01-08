208a5ee1d4b7fb70e37269dc9fdcaf714ac2868e
sbreno001 <sbreno.silva03@gmail.com>
2026-01-07 21:42:54 -0300

    feat: init project

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        $name = 'Breno';
+        $habits = ['Ler', 'Correr', 'Estudar', 'Viajar'];
+
+        return view('home', [
+            'name' => $name,
+            'habits' => $habits,
+        ]);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<h1>
+    welcome to the home page
+</h1>
+<p>
+    Olá, {{ $name }}
+</p>
+<p>
+    Seus habitos são:
+</p>
+<ul>
+    @foreach($habits as $item)
+        <li>
+            {{ $item }}
+        </li>
+    @endforeach
+</ul>
